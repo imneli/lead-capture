a8cb2ba9ebee42e58775782a816b8f434f8b7df9
nekcyag <contatodoneli@gmail.com>
2025-01-17 17:42:54 -0300

    send with your own whatsapp

---
+++ whatsappFree.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('results.json', 'utf-8'));
+
+async function enviarMensagemWhatsApp(phone, message) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://web.whatsapp.com');
+  console.log('Escaneie o QR Code do WhatsApp Web no navegador.');
+
+  await page.waitForSelector('div[data-tab="3"]', { visible: true, timeout: 60000 });
+
+  await page.click('div[data-tab="3"]');
+  await page.type('div[data-tab="3"]', phone);
+  await page.waitForTimeout(2000); 
+  await page.keyboard.press('Enter');
+  await page.waitForTimeout(5000); 
+
+  const messageBoxSelector = 'div[data-tab="10"]';
+  await page.waitForSelector(messageBoxSelector, { visible: true });
+  await page.click(messageBoxSelector);
+  await page.type(messageBoxSelector, message);
+  await page.keyboard.press('Enter');
+
+  console.log(`Mensagem enviada para ${phone}: ${message}`);
+
+  await browser.close();
+}
+
+async function enviarMensagensAutomaticas() {
+  for (const estabelecimento of data) {
+    if (estabelecimento.phone) {
+      const mensagem = `Olá, ${estabelecimento.title}! Sou um desenvolvedor autônomo e faço projetos Freelance, ofereço serviços de criação de sites profissionais e sistemas web para empresas, gostaria de saber se tem interesse, tenho portfolio de alguns sistemas que já programei, caso queira saber mais aguardo alguma resposta, obrigado!`;
+      await enviarMensagemWhatsApp(estabelecimento.phone, mensagem);
+      await new Promise((resolve) => setTimeout(resolve, 10000)); 
+    }
+  }
+}
+
+enviarMensagensAutomaticas().catch(console.error);
